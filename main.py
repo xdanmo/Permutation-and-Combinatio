1cc3561e9f575c31784fb4db381833de4d25a8eb
XDANMO <101450888+xdanmo@users.noreply.github.com>
2024-12-07 18:06:32 +0530

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import math
 
+a = input("[1] for Permutation\n[2] for Combination\n[3] for both\nEnter: ")
+
 while True:
-    a = input('Enter "P" for Permutation and "C" for Combination: ').lower()
-        
-    if a == 'p':
-        n = int(input('Enter the value of n: '))
-        r = int(input('Enter the value of r: '))
-        npr = math.factorial(n)/math.factorial(n-r)
-        print(npr)
-        break
     
-    if a == 'c':
-        n = int(input('Enter the value of n: '))
-        r = int(input('Enter the value of r: '))
-        ncr = math.factorial(n)/(math.factorial(r)*math.factorial(n-r))
-        print(ncr)
+    if a=='1':
+        n = int(input('Enter the value of "n": '))
+        r = int(input('Enter the value of "r": '))
+        nr = n-r
+
+        NPR = math.factorial(n)/(math.factorial(nr))
+        print('NPR = ',NPR)
         break
 
+    elif a=='2':
+        n = int(input('Enter the value of "n": '))
+        r = int(input('Enter the value of "r": '))
+        nr = n-r
+
+        NCR = math.factorial(n)/(math.factorial(r)*math.factorial(nr))
+        print('NCR = ',NCR)
+        break
+
+    elif a=='3':
+        n = int(input('Enter the value of "n": '))
+        r = int(input('Enter the value of "r": '))
+        nr = n-r
+        
+        NPR = math.factorial(n)/(math.factorial(nr))
+        print('NPR = ',NPR)
+        NCR = math.factorial(n)/(math.factorial(r)*math.factorial(nr))
+        print('NCR = ',NCR)
+        break
+        
     else:
-        print('Invalid input')
-    
+        print('Invalid input, please try again.')
+
+    a = input('Enter: ')
+        
+
+        
+        
